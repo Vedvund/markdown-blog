6ab8b65b28f98e4039e416617bce3bb15a9d91b9
vedvund <vedvund@gmail.com>
2022-05-27 13:12:22 +0530

    Feat edit and display blog

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles')
 const Article = require('./models/article')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
 
@@ -15,6 +16,7 @@ main().catch(err => console.log(err));
 // express settings
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 
 app.get('/', async (req, res) => {
